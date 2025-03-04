ba5d643a459a2f4f1489cd6b03c3b7ced7bd34d3
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-03-04 14:17:03 +0100

    product

--- src/app/components/SlideImages/SlideImages.jsx
+++ src/app/components/SlideImages/SlideImages.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import style from "./SlideImages.module.css";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "next/image";
+import image1 from "../../../../public/category/1.jpeg?as=webp&w=500&q=75";
+import image2 from "../../../../public/category/2.jpeg?as=webp&w=500&q=75";
 
 // Import Swiper styles
 import "swiper/css";
@@ -9,41 +13,42 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 const MySwiperComponent = () => {
+  const images = [
+    {
+      src: image1,
+      alt: "image 1",
+      key: "1",
+    },
+    {
+      src: image2,
+      alt: "image 2",
+      key: "2",
+    },
+  ];
+
   return (
     <Swiper
+      className={style.images}
       // Install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={20} // Space between slides
       slidesPerView={1} // Number of slides per view
-      navigation={{
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      }} // Enable navigation buttons
       pagination={{ clickable: true }} // Enable clickable pagination
       scrollbar={{ draggable: true }} // Enable draggable scrollbar
       onSwiper={(swiper) => console.log(swiper)} // Callback when Swiper is initialized
       onSlideChange={() => console.log("slide change")} // Callback when slide changes
-      style={{
-        height: "190px",
-        width: "50%",
-        "@media (min-width: 1024px)": {
-          height: "250px",
-          width: "50%",
-        },
-      }}
     >
-      <SwiperSlide>
-        <img src="https://picsum.photos/200/150?random=1" alt="image 1" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="https://picsum.photos/200/150?random=2" alt="image 2" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="https://picsum.photos/200/150?random=3" alt="image 3" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="https://picsum.photos/200/150?random=4" alt="image 4" />
-      </SwiperSlide>
+      {images.map((image) => (
+        <SwiperSlide key={image.key}>
+          <Image
+            className={style.image}
+            src={image.src}
+            alt={image.alt}
+            width={500} // Remplacez par la largeur réelle de l'image
+            style={{ width: "100%", height: "auto", borderRadius: "10px" }}
+          />
+        </SwiperSlide>
+      ))}
       {/* Add more slides as needed */}
     </Swiper>
   );
--- src/app/produitPage/[id]/page.jsx
+++ src/app/produitPage/[id]/page.jsx
@@ -6,26 +6,89 @@ import Slideshow from "@/app/components/SlideImages/SlideImages";
 const page = () => {
   return (
     <div className={style.container}>
-      <Slideshow />
+      <div className={style.slide}>
+        <Slideshow />
+      </div>
+
       <div className={style.description}>
-        <h1>titre</h1>
-        <p>description</p>
+        <div className={style.TitleDesc}>
+          <h1 className={style.title}>Formes & Couleurs</h1>
+          <p>
+            description pour le produits en question qui répond à la catégorie
+            et thématique choisis par l'utilisateur
+          </p>
+        </div>
+
         <div className={style.btnFormat}>
           <button>PDF</button>
           <button>Physique</button>
         </div>
-        <span>19,90€</span>
-        <div>
-          <span>Quantité: 1 dans le panier</span>
 
-          <div className={style.quantityBtn}>
-            <button style={{ border: "none" }}>-</button>
-            <span style={{ margin: "0 10px" }}>1</span>
-            <button style={{ border: "none" }}>+</button>
+        <div className={style.priceContainer}>
+          <span className={style.price}>19,90€</span>
+          <div>
+            <span className={style.quantity}>Quantité disponible : 2</span>
+
+            <div className={style.quantityBtn}>
+              <button
+                style={{
+                  border: "none",
+                  padding: "0.5rem",
+                  fontSize: "1.5rem",
+                  backgroundColor: "transparent",
+                  cursor: "pointer",
+                }}
+              >
+                -
+              </button>
+              <button
+                style={{
+                  border: "none",
+                  padding: "0.5rem 1.5rem",
+                  backgroundColor: "transparent",
+                  disabled: true,
+                  fontSize: "1rem",
+                }}
+              >
+                1
+              </button>
+              <button
+                style={{
+                  border: "none",
+                  padding: "0.5rem",
+                  fontSize: "1.5rem",
+                  backgroundColor: "transparent",
+                  cursor: "pointer",
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className={style.actionBtn}>
+            <button
+              className={style.addToCartBtn}
+              style={{
+                backgroundColor: "#2A628F",
+                border: "none",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+              }}
+            >
+              Ajouter au panier
+            </button>
+            <button
+              className={style.addToCartBtn}
+              style={{
+                backgroundColor: "#E76F51",
+                border: "none",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+              }}
+            >
+              Acheter
+            </button>
           </div>
         </div>
       </div>
-      <button className={style.addToCartBtn}>Ajouter au panier</button>
     </div>
   );
 };
