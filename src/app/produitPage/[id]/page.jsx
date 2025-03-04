541a51d6f36ea560af42dd6c7638e2ca053af0cb
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-03-04 14:30:09 +0100

    modif

--- src/app/produitPage/[id]/page.jsx
+++ src/app/produitPage/[id]/page.jsx
@@ -5,87 +5,92 @@ import Slideshow from "@/app/components/SlideImages/SlideImages";
 
 const page = () => {
   return (
-    <div className={style.container}>
-      <div className={style.slide}>
-        <Slideshow />
-      </div>
-
-      <div className={style.description}>
-        <div className={style.TitleDesc}>
-          <h1 className={style.title}>Formes & Couleurs</h1>
-          <p>
-            description pour le produits en question qui répond à la catégorie
-            et thématique choisis par l'utilisateur
-          </p>
+    <div className={style.mainContainer}>
+      <div className={style.container}>
+        <div className={style.slide}>
+          <Slideshow />
         </div>
 
-        <div className={style.btnFormat}>
-          <button>PDF</button>
-          <button>Physique</button>
-        </div>
+        <div className={style.description}>
+          <div className={style.TitleDesc}>
+            <h1 className={style.title}>Formes & Couleurs</h1>
+            <p>
+              description pour le produits en question qui répond à la catégorie
+              et thématique choisis par l'utilisateur description pour le
+              produits en question qui répond à la catégorie et thématique
+              choisis par l'utilisateur description pour le produits en question
+              qui répond à
+            </p>
+          </div>
 
-        <div className={style.priceContainer}>
-          <span className={style.price}>19,90€</span>
-          <div>
-            <span className={style.quantity}>Quantité disponible : 2</span>
+          <div className={style.btnFormat}>
+            <button>PDF</button>
+            <button>Physique</button>
+          </div>
 
-            <div className={style.quantityBtn}>
+          <div className={style.priceContainer}>
+            <span className={style.price}>19,90€</span>
+            <div>
+              <span className={style.quantity}>Quantité disponible : 2</span>
+
+              <div className={style.quantityBtn}>
+                <button
+                  style={{
+                    border: "none",
+                    padding: "0.5rem",
+                    fontSize: "1.5rem",
+                    backgroundColor: "transparent",
+                    cursor: "pointer",
+                  }}
+                >
+                  -
+                </button>
+                <button
+                  style={{
+                    border: "none",
+                    padding: "0.5rem 1.5rem",
+                    backgroundColor: "transparent",
+                    disabled: true,
+                    fontSize: "1rem",
+                  }}
+                >
+                  1
+                </button>
+                <button
+                  style={{
+                    border: "none",
+                    padding: "0.5rem",
+                    fontSize: "1.5rem",
+                    backgroundColor: "transparent",
+                    cursor: "pointer",
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className={style.actionBtn}>
               <button
+                className={style.addToCartBtn}
                 style={{
+                  backgroundColor: "#2A628F",
                   border: "none",
-                  padding: "0.5rem",
-                  fontSize: "1.5rem",
-                  backgroundColor: "transparent",
-                  cursor: "pointer",
+                  boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
                 }}
               >
-                -
+                Ajouter au panier
               </button>
               <button
+                className={style.addToCartBtn}
                 style={{
+                  backgroundColor: "#E76F51",
                   border: "none",
-                  padding: "0.5rem 1.5rem",
-                  backgroundColor: "transparent",
-                  disabled: true,
-                  fontSize: "1rem",
+                  boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
                 }}
               >
-                1
-              </button>
-              <button
-                style={{
-                  border: "none",
-                  padding: "0.5rem",
-                  fontSize: "1.5rem",
-                  backgroundColor: "transparent",
-                  cursor: "pointer",
-                }}
-              >
-                +
+                Acheter
               </button>
             </div>
-          </div>
-          <div className={style.actionBtn}>
-            <button
-              className={style.addToCartBtn}
-              style={{
-                backgroundColor: "#2A628F",
-                border: "none",
-                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-              }}
-            >
-              Ajouter au panier
-            </button>
-            <button
-              className={style.addToCartBtn}
-              style={{
-                backgroundColor: "#E76F51",
-                border: "none",
-                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-              }}
-            >
-              Acheter
-            </button>
           </div>
         </div>
       </div>
