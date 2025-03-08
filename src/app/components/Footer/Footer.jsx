ee65006a8406cbee3a06c13c4adedac0474f8708
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-03-08 15:37:44 +0100

    CGV

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -7,6 +7,16 @@ const Footer = () => {
   const date = new Date().getFullYear();
   return (
     <footer className={style.container}>
+      <p style={{ fontSize: "10px", color: "black" }}>
+        📄{" "}
+        <a
+          href="/CGV.pdf"
+          download
+          aria-label="Télécharger les Conditions Générales de Vente"
+        >
+          Télécharger nos Conditions Générales de Vente (CGV)
+        </a>
+      </p>
       <p style={{ fontSize: "10px" }}>
         &copy; {date} GaelVbn - Tous droits réservés
       </p>
