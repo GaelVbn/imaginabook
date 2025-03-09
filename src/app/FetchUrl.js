c8496cf12efd1b367639b12779528a258bde1fe7
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-03-09 19:17:16 +0100

    fetchUrl

--- src/app/FetchUrl.js
+++ src/app/FetchUrl.js
@@ -1,3 +1,4 @@
-export const fetchUrl = "https://imaginabook-backend-production.up.railway.app";
+export const fetchUrl =
+  "https://imaginabook-backend-production.up.railway.app/api";
 //https://imaginabook-backend-production.up.railway.app
 //http://localhost:3005/api
