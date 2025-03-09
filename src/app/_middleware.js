3d8af53aaaec4558596673d782972db3694e0bb7
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-03-09 02:12:07 +0100

    middleware cors

--- src/app/_middleware.js
+++ src/app/_middleware.js
@@ -1,10 +1,9 @@
-// pages/api/_middleware.js
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
   const res = NextResponse.next();
 
-  // Configurer les en-têtes CORS
+  // Appliquer les en-têtes CORS pour les requêtes provenant de ton frontend
   res.headers.set("Access-Control-Allow-Origin", "https://www.imaginabook.com");
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.headers.set(
@@ -12,11 +11,22 @@ export function middleware(req) {
     "Content-Type, Authorization"
   );
 
-  // Gérer la requête OPTIONS pour le pré-vol
+  // Gérer les requêtes OPTIONS pour les pré-vols
   if (req.method === "OPTIONS") {
-    res.headers.set("Access-Control-Max-Age", "86400");
-    res.headers.set("Access-Control-Allow-Origin", "*");
+    // Répondre avec un statut 204 pour la pré-requête OPTIONS (en-tête CORS valide)
+    res.headers.set("Access-Control-Max-Age", "86400"); // Cache pour 1 jour
     return res.status(204).end();
+  }
+
+  // Si la requête est un POST pour /api/checkout_sessions, s'assurer que les en-têtes sont là aussi
+  if (
+    req.method === "POST" &&
+    req.nextUrl.pathname === "/api/checkout_sessions"
+  ) {
+    res.headers.set(
+      "Access-Control-Allow-Origin",
+      "https://www.imaginabook.com"
+    );
   }
 
   return res;
