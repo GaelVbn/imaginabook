9ac93f43ec4ac1c835c53b59499bf10da9a965c1
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-03-10 15:43:48 +0100

    log enlever

--- src/app/api/stripe.js
+++ src/app/api/stripe.js
@@ -1,7 +1,5 @@
 import Stripe from "stripe";
 
-console.log("STRIPE_SECRET_KEY:", process.env.STRIPE_SECRET_KEY); // Ajoute ce log pour vérifier la clé
-
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
   apiVersion: "2023-10-16",
 });
