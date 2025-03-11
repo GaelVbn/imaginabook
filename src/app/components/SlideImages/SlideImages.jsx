e2e35a652c412ffdafb9047189f9e77e53fe75fa
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-03-11 20:44:32 +0100

    image priority

--- src/app/components/SlideImages/SlideImages.jsx
+++ src/app/components/SlideImages/SlideImages.jsx
@@ -30,7 +30,7 @@ const MySwiperComponent = ({ imgPhysique, imgPDF, buttons }) => {
                 alt={"imagesPDF"}
                 width={500} // Remplacez par la largeur réelle de l'image
                 height={500}
-                loading="lazy"
+                priority
               />
             </SwiperSlide>
           ))
@@ -44,7 +44,7 @@ const MySwiperComponent = ({ imgPhysique, imgPDF, buttons }) => {
                 width={500} // Remplacez par la largeur réelle de l'image
                 height={500}
                 style={{ width: "100%", height: "auto", borderRadius: "10px" }}
-                loading="lazy"
+                priority
               />
             </SwiperSlide>
           ))
