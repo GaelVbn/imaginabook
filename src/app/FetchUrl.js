8334d406a6b6248904746e355e4d6205aead75b1
Gaël Vanbeveren <163444311+GaelVbn@users.noreply.github.com>
2025-07-07 10:35:00 +0200

    Update FetchUrl.js

--- src/app/FetchUrl.js
+++ src/app/FetchUrl.js
@@ -1,4 +1,4 @@
 export const fetchUrl =
-  "https://imaginabook-backend-production.up.railway.app/api";
+  "https://round-averil-gaelvbn-50ef659b.koyeb.app/api";
 //https://imaginabook-backend-production.up.railway.app
 //http://localhost:3005/api
